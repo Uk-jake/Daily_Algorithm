58f9f92dcefe4d101c92990ca67ca079df2ca86a
Uk-jake <mag0225@naver.com>
2024-07-10 16:37:35 +0900

    Refactor Programmers 다리를 지나는 트럭

--- 7월 밀라 알고리즘 스터디/7월 1주차/Programmers_다리를지나는트럭_240710.py
+++ 7월 밀라 알고리즘 스터디/7월 1주차/Programmers_다리를지나는트럭_240710.py
@@ -1,46 +1,36 @@
 import queue
 
+
 def solution(bridge_length, weight, truck_weights):
     bridge_queue = queue.Queue()
-    answer_list = []
     answer = 0
-    idx = 0
-    bridge_weight = 0    
-    
+    bridge_weight = 0
+
     # 다리 길이 만큼 큐의 초기 값을 0으로 선언
     for i in range(bridge_length):
         bridge_queue.put(0)
-    
-    # answer_list에 모든 트럭이 도착하였다면 while문 정지
-    # while문이 한번 씩 반복 될 때 마다 1초가 지날 때 발생하는 일들을 작성함
-    while len(answer_list) != len(truck_weights):
-        
+
+    while len(truck_weights) > 0 :
+
         # 1초 증가
         answer += 1
-        
+
         # 도착한 트럭은 bridge_queue에서 삭제, bridge_weight(현재 다리에 올라가있는 트럭들의 무게)에서 차감
-        arrvied_truck = bridge_queue.get()
-        bridge_weight -= arrvied_truck
-        
-        # 도착한 트럭이 0이면 아무것도 도착하지 않은 것
-        # 0이 아니면 arrived_list에 추가
-        if arrvied_truck != 0:
-            answer_list.append(arrvied_truck)
-        
-        bridge_weight += truck_weights[idx]
-        
-        # 다음 순서의 트럭이 올라갔을 때 무게가 초가된다면 queue에 0 값 추가
-        if bridge_weight > weight:
+        bridge_weight -= bridge_queue.get()
+
+        if bridge_weight + truck_weights[0] <= weight:
+            bridge_weight += truck_weights[0]
+            print(f"truck_weight[0] : {truck_weights[0]}")
+            bridge_queue.put(truck_weights.pop(0))
+        else :
             bridge_queue.put(0)
-            bridge_weight -= truck_weights[idx]
-        # 초과되지 않는다면 queue에 트럭 추가
-        elif bridge_weight <= weight:
-            bridge_queue.put(truck_weights[idx])
-            idx += 1
-            if idx >= len(truck_weights):
-                idx -= 1
+            
+        # print(f"bridge_queue : {bridge_queue}")
+        print(f"answer : {answer}")
+
+    answer += bridge_length
     
     return answer
 
 
-print( solution(2, 10, [7, 4, 5, 6]) )
+print(solution(2, 10, [7, 4, 5, 6]))
