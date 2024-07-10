e31850dce22e9d2efe40c98d0323600fe9fd25df
Uk-jake <mag0225@naver.com>
2024-07-10 16:56:50 +0900

    Refactor Programmers 다리를 지나는 트럭

--- 7월 밀라 알고리즘 스터디/7월 1주차/Programmers_다리를지나는트럭_240710.py
+++ 7월 밀라 알고리즘 스터디/7월 1주차/Programmers_다리를지나는트럭_240710.py
@@ -1,6 +1,5 @@
 import queue
 
-
 def solution(bridge_length, weight, truck_weights):
     bridge_queue = queue.Queue()
     answer = 0
@@ -15,22 +14,18 @@ def solution(bridge_length, weight, truck_weights):
         # 1초 증가
         answer += 1
 
-        # 도착한 트럭은 bridge_queue에서 삭제, bridge_weight(현재 다리에 올라가있는 트럭들의 무게)에서 차감
         bridge_weight -= bridge_queue.get()
 
         if bridge_weight + truck_weights[0] <= weight:
             bridge_weight += truck_weights[0]
-            print(f"truck_weight[0] : {truck_weights[0]}")
             bridge_queue.put(truck_weights.pop(0))
         else :
             bridge_queue.put(0)
             
-        # print(f"bridge_queue : {bridge_queue}")
         print(f"answer : {answer}")
 
     answer += bridge_length
     
     return answer
-
 
 print(solution(2, 10, [7, 4, 5, 6]))
