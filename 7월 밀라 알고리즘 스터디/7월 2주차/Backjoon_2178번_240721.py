213190f9ab638d4c29db5ad071cbb90f0f409a77
Uk-jake <mag0225@naver.com>
2024-07-22 14:52:33 +0900

    Add Backjoon 15663

--- 7월 밀라 알고리즘 스터디/7월 2주차/Backjoon_2178번_240721.py
+++ 7월 밀라 알고리즘 스터디/7월 2주차/Backjoon_2178번_240721.py
@@ -1,46 +1,52 @@
 import sys
 from collections import deque
 
-dx = [1,-1,0,0]
-dy = [0,0,1,-1]
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
 
+# BFS 함수
 def bfs(i, j):
-    
     que = deque()
-    que.append((i,j))
+    # 시작 인덱스 que에 deque
+    que.append((i, j))
+    # 시작 인덱스 방문 처리
     visited[i][j] = 1
-    
+
+    # que에 데이터가 없을 때까지 반복
     while que:
-        x , y = que.popleft()
-        
+        x, y = que.popleft()
+
         for i in range(4):
+            # 상하좌우 이동
             nx = x + dx[i]
             ny = y + dy[i]
-            
+
+            # 초과 인덱스 처리
             if nx < 0 or nx >= N or ny < 0 or ny >= M:
                 continue
-            
+
+            # 인접한 데이터가 이동할 수 있고 방문하지 않은 곳이라면
             if graph[nx][ny] == 1 and visited[nx][ny] == 0:
                 visited[nx][ny] = visited[x][y] + 1
-                que.append((nx,ny))
-    
-    
+                que.append((nx, ny))
+
 
 input = sys.stdin.readline
 
 # N - 미로의 가로 크기, M - 미로의 세로 크기
 N, M = map(int, input().strip().split())
 
+# 미로 리스트 선언
 graph = []
+
+# 방문 여부 및 최단 거리 리스트 선언
 visited = [[0 for _ in range(M)] for _ in range(N)]
 
 
 for i in range(N):
     graph.append(list(map(int, input().strip())))
-    
-bfs(0,0)
 
-for line in visited:
-    print(line)
-    
+# (0,0)에서 bfs 시작
+bfs(0, 0)
+
 print(visited[N-1][M-1])
---
+++ 7월 밀라 알고리즘 스터디/7월 3주차/Backjoon_15663번_240722.py
@@ -0,0 +1,39 @@
+import sys
+
+input = sys.stdin.readline
+
+# 백트래킹 함수
+def backtracking( length, tmp_list ):
+    # 순열의 개수가 M개이면 answer에 추가
+    if length == M:
+        answer.append(tmp_list)
+        return
+    
+    prev = 0
+    # N번 반복
+    for i in range(N):
+        # 방문하지 않은 곳이고 이전 사용한 값이 같이 않으면 순열 추가
+        if visited[i] == 0 and prev != input_list[i]:
+            prev = input_list[i]
+            visited[i] = 1
+            backtracking(length + 1, tmp_list+[input_list[i]])
+            visited[i] = 0
+        
+# N과 M 입력
+N, M = map(int, input().strip().split())
+
+# 자연수 N개 입력
+input_list = list(map(int, input().strip().split()))
+# 오름차순 정렬
+input_list.sort()
+
+# 방문 여부
+visited = [0] * N
+answer = []
+
+# 백트래킹 시작
+backtracking(0, [])
+
+# 순열 출력
+for line in answer:
+    print(*line)
